b978d83f553aee74341a87da226c2704583e35ad
Yash <77000423+yashk0311@users.noreply.github.com>
2022-11-25 03:12:15 +0530

    Update tester.py
    Changed the value for both the testers.

--- tester.py
+++ tester.py
@@ -7,14 +7,14 @@ from prob2 import pred
 class Calculator(unittest.TestCase):
 
     def test_OddEven(self):
-        x = 12
+        x = 11
         res = OddEven(x)
-        self.assertEqual(res, 24)
+        self.assertEqual(res, 12)
 
     def test_pred(self):
-        x = 9
+        x = 99
         res1 = pred(x)
-        self.assertEqual(res1, 8)
+        self.assertEqual(res1, 98)
     
     
 
